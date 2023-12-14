30c34fb8f821d66b2e3ced79bccb5bea16a00013
wbvi <wb_enduo@126.com>
2023-12-14 15:13:39 +0800

    更新

--- bin/autoDeploy.bat
+++ bin/autoDeploy.bat
@@ -3,8 +3,8 @@ cd docs/.vitepress/dist
 
 git init
 git add -A
-git commit -m "auto construct docs"
-
+git commit -m "auto construct docs3"
+@REM 上传到分支dh-pages
 git push -f https://github.com/wbvi/wbvi.github.io master:dh-pages
 
 @REM 这里dis-pages是一个分支
