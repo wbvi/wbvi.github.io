8d8e8b42fe7cbeecc050029ed4a817610fc2bb23
wbvi <wb_enduo@126.com>
2023-12-14 16:24:00 +0800

    update

---
+++ bin/gengxin.sh
@@ -0,0 +1,33 @@
+# /bin/bash
+
+# 确保脚本抛出遇到的错误
+set -e
+
+
+# cd ../../../
+git add .
+git commit -m "update"
+git push
+
+# 重新打包组件库
+
+pnpm build
+
+# 打包生成静态文件
+pnpm docs:build
+
+# 进入待发布的 dist/ 目录
+cd docs/.vitepress/dist
+
+git init
+git add .
+git commit -m 'deploy'
+
+# 部署到 https://.github.io/
+git push -f git@github.com:wbvi/wbvi.github.io.git master:dh-pages
+
+# 提交所有代码到github
+# cd ../../../
+# git add .
+# git cm -m 'update'
+# git push
